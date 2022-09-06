3d25fd6bf8f7f0465d8c0d99b8d3e806d21d80b3
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2022-09-06 08:55:36 +0700

    Added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='px-20 py-10 font-lato space-y-4 bg-[#F3F7FB] h-[79.2vh] flex flex-col justify-center items-center'>
+      <h1 className='font-bold text-4xl'>Home Page</h1>
     </div>
   );
 };
