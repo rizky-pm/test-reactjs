149a568aee39145c84e3caf0ea619812809aef80
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2022-09-06 08:53:08 +0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Routers from './routes';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routers />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='text-center bg-[#1D4279] text-white py-2'>
+      <span>Kandidat : Rizky Putra Mahendra</span>
+    </footer>
+  );
+};
+
+export default Footer;
